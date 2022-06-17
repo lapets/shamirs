1df721250fbd67f87984fddce08132f09dec172d
Andrei Lapets <a@lapets.io>
2022-06-17 18:28:16 -0400

    Update package file, package version, and dependency version lock.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-    name             = 'shamirs',
-    version          = '0.1.0.0',
-    packages         = ['shamirs',],
-    install_requires = ['lagrange',],
-    license          = 'MIT',
-    url              = 'https://github.com/lapets/lagrange',
-    author           = 'Andrei Lapets',
-    author_email     = 'a@lapets.io',
-    description      = 'Python library with a minimal native implementation of Shamir\'s Secret Sharing algorithm.',
-    long_description = open('README.rst').read(),
+    name='shamirs',
+    version='0.2.0',
+    packages=['shamirs',],
+    install_requires=[
+        'lagrange~=1.0',
+    ],
+    license='MIT',
+    url='https://github.com/lapets/lagrange',
+    author='Andrei Lapets',
+    author_email='a@lapets.io',
+    description='Minimal pure-Python implementation of Shamir\'s Secret Sharing scheme.',
+    long_description=open('README.rst').read(),
 )
